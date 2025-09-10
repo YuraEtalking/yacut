ec85aed899b0f598f963ca1795cbb0849d5f1f87
Yuri Ryzhkov <yura.ryzhkov.photo@yandex.ru>
2025-09-10 14:38:58 +0300

    тесты зелень

--- yacut/constants.py
+++ yacut/constants.py
@@ -7,6 +7,7 @@ import re
 MAX_LENGTH_SHORT_FIELD: int = 16
 MAX_LENGTH_ORIGINAL_FIELD: int = 1999
 MIN_LENGTH_FIELD: int = 1
+ENCODE: str = 'utf-8'
 
 # Константы для utility
 MAX_ATTEMPTS: int = 10
@@ -14,9 +15,9 @@ LENGTH_GENERATED_SHORTCODE: int = 6
 SALT_SIZE_BYTES: int = 3
 
 # Константы для валидаторов.
-SHORT_PATTERN = re.compile(r"[A-Za-z0-9]+", re.ASCII)
+SHORT_PATTERN: re.Pattern[str] = re.compile(r"[A-Za-z0-9]+", re.ASCII)
 # Паттерн валидатора URL из WTForms, что бы соответствовать форме.
-URL_PATTERN = re.compile(
+URL_PATTERN: re.Pattern[str] = re.compile(
     r"^[a-z]+://"
     r"(?P<host>[^\/\?:]+)"
     r"(?P<port>:[0-9]+)?"
